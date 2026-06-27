5f7a29c68ab05f6dd194f5f2fb9384e241b7e8b2
colin <suanhuahua9@gmail.com>
2026-06-28 00:53:05 +0800

    feat: 浏览器标题改为雪笠微光

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
+import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import { GeistSans } from "geist/font/sans";
 
 import "./globals.css";
 import "react-image-crop/dist/ReactCrop.css";
+export const metadata: Metadata = {
+  title: "雪笠微光",
+  description: "雪笠微光",
+};
 
 export default function RootLayout({
   children,
